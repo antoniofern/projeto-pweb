1f9802039e649e19fa145629f053931450b412d1
Jhonatas <costajhonatas9@gmail.com>
2019-12-12 18:11:26 -0300

    funcao insertNewRecord.js

--- antoniofern/src/script.js
+++ antoniofern/src/script.js
@@ -123,4 +123,25 @@ function readFormData() {
     formData["tipo"] = document.getElementById("tipo").value;
     return formData;
 }
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.idQuestion;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.descricao;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.nivel;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.fonte;
+    cell5 = newRow.insertCell(4);
+    cell5.innerHTML = data.ano;
+    cell6 = newRow.insertCell(5);
+    cell6.innerHTML = data.area;
+    cell7 = newRow.insertCell(6);
+    cell7.innerHTML = data.tipo;
+    cell8 = newRow.insertCell(7);
+    cell8.innerHTML = `<a onClick="onEdit(this)">Editar</a>
+                       <a onClick="onDelete(this)">Deletar</a>`;
+}
 
