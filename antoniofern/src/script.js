aa813c882c34339ac2148f6ed6a45e64c412b295
Jhonatas <costajhonatas9@gmail.com>
2019-12-12 18:07:59 -0300

    fucao onFormSubmit.js

--- antoniofern/src/script.js
+++ antoniofern/src/script.js
@@ -97,3 +97,17 @@ $('#quiz').quiz({
     }
   ]
 });
+
+
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
